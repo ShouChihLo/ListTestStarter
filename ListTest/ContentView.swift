9389d677cf985eb3eae179b5c99a76bf6ecaf531
sclo <sclo@gms.ndhu.edu.tw>
2023-07-07 16:02:51 +0800

    Initial Commit

---
+++ ListTest/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ListTest
+//
+//  Created by 羅壽之 on 2023/1/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    let restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
+    
+    var body: some View {
+       Text("Get ready")
+
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
